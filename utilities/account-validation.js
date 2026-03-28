59cee49f784a0f8ccac8a098a5eb2d9b7e7675fd
DollEunAnn <euniceannhdollete@gmail.com>
2026-03-28 22:04:39 +0800

    added password hash and login success

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,7 +1,7 @@
 const utilities = require(".")
-  const { body, validationResult } = require("express-validator")
-  const accountModel = require("../models/account-model")
-  const validate = {}
+const { body, validationResult } = require("express-validator")
+const accountModel = require("../models/account-model")
+const validate = {}
 
   /*  **********************************
   *  Registration Data Validation Rules
